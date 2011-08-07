bb333f03231a15ee2c97e233300fd560efdf116f
Lyza Danger Gardner <lyza@lyza.com>
2011-08-07 13:57:49 -0700

    Initial commit of steps 1 through 3 with concomitant, but probably inadequate, README files

--- step_1/index.php
+++ step_1/index.php
@@ -1,3 +1,4 @@
+<?php require_once('redirect.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,78 +8,7 @@
   <link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 <body>
-  <div class="header">
-    <h1>Creature Comforts</h1>
-    <h2>Agent Portal</h2>
-  </div>
-
-  <div class="greeting">
-    <h3>Welcome back, Dr. Jessica Evans</h3>
-  </div>
-  
-  <div class="tools">
-    <ul>
-      <li><a href="#">Messages (2)</a></li>
-      <li><a href="#">Your Schedule</a></li>
-      <li><a href="#">Request Supplies</a></li>
-      <li><a href="#">Request Personnel</a></li>
-      <li><a href="#">Call HQ</a></li>
-    </ul>
-  </div>
-  
-    <div class="status_message">
-    <p>February 4: Loading supplies and contacting personnel to assist with flood event in Bangladesh. Contact HQ if you are currently unscheduled and available to assist in the effort.</p>
-    </div>
-    
-    <div class="dashboard">
-      <div class="event odd">
-        <div class="event_meta">
-          <div class="event_type">Message</div>
-          <div class="event_time">Feb 3 8:54PM EST</div>
-        </div>
-        <div class="event_details">  
-          <div class="event_subject"><a href="#">Supply Request #493-C4 Approved</a></div>
-          <div class="event_summary">Hi, Jess, Good news! I wanted to let you know that we were successful in tracking down those bottles…</div>
-        </div>
-      </div>
-      
-      <div class="event even">
-        <div class="event_meta">
-          <div class="event_type">Message</div>
-          <div class="event_time">Feb 3 1:47PM EST</div>
-        </div>
-        <div class="event_details">    
-          <div class="event_subject"><a href="#">Supply Request #493-C4 Received</a></div>
-          <div class="event_summary">This is an automated message to confirm that your recent Supply Request has been received and is in process…</div>
-        </div>
-      </div>
-      
-      <div class="event odd">
-        <div class="event_meta">
-          <div class="event_type">Event Scheduled</div>
-          <div class="event_time">Feb 3 8:22AM EST</div>
-        </div>
-        <div class="event_details">
-          <div class="event_subject"><a href="#">Itinerary 39924 Approved LAX -> DAC</a></div>
-          <div class="event_summary">Your itinerary for event: "Bangladesh Flood Event" has been approved. Your calendar has been updated…</div>
-        </div>
-      </div>
-          
-      <div class="event even">
-        <div class="event_meta">
-          <div class="event_type">Personnel Event</div>
-          <div class="event_time">Feb 2 9:23PM EST</div>
-        </div>
-        <div class="event_details">      
-          <div class="event_subject"><a href="#">Re: Personnel Confirmation 03/05 - 03/15</a></div>
-          <div class="event_summary">Jessica! Thanks so much for committing to this operation! I think Dr. Madling is going too and…</div>
-        </div>
-      </div>
-    <div class="morelink"><a href="#">More &gt;&gt;</a></div> 
-    </div> <!-- /.dashboard -->
-
-  <div class="footer">
-    
-  </div>
+  <h1>Ostensible Desktop Landing Page</h1>
+  <h2>Use index_mobile.html instead</h2>
 </body>
 </html>
