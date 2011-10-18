98ef0fd14f5f9bf8bdfcd31cf7953b4df671096b
Lyza Danger Gardner <lyza@lyza.com>
2011-10-17 17:58:15 -0700

    More prevaricating about builds and zips and .gitignores.

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p chapter3
 cd chapter3
-cp -LR ../01_basic_redirect/* .
+cp -LR ../01_basic_redirect/* . # Starting point for exercises
 cd ..
-zip -r chapter3.zip chapter3
+zip -qr chapter3.zip chapter3
